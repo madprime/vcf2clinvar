d5bbf6df2902c6cabe9ef1894cfac527e90fa32a
Mad Price Ball <mpball@gmail.com>
2018-01-21 07:08:39 -0800

    Fix email info

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='vcf2clinvar',
-    version='0.1.5.0',
+    version='0.1.5.1',
     description='Match a personal genome VCF datafile to ClinVar VCF',
     url='https://github.com/madprime/vcf2clinvar',
     author='Mad Price Ball',
-    author_email='pgp-informatics@arvados.org',
+    author_email='mpball+pypi@gmail.com',
     license='MIT',
 
     classifiers=[
